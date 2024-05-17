5e1b5f4ce41122313c240e5d3e9883af674b6655
Nortel Data <89657497+northportio@users.noreply.github.com>
2024-05-16 20:28:26 -0400

    Update carcontroller.py

--- selfdrive/car/ford/carcontroller.py
+++ selfdrive/car/ford/carcontroller.py
@@ -151,6 +151,16 @@ class CarController:
         self.precharge_actutator_stdDevHigh = 0.05
         self.app_PC_percentage = 0.5 # what percentage of apply_curvature is derived from predicted curvature
 
+        elif self.CP.carFingerprint == "FORD ESCAPE 2023 REFRESH":
+        print(f'Matched carFingerprint: {self.CP.carFingerprint}')
+        self.brake_actutator_target = -0.1
+        self.brake_actutator_stdDevLow = 0.00
+        self.brake_actutator_stdDevHigh = 0.05
+        self.precharge_actutator_target = -0.1
+        self.precharge_actutator_stdDevLow = 0.0
+        self.precharge_actutator_stdDevHigh = 0.05
+        self.app_PC_percentage = 0.5 # what percentage of apply_curvature is derived from predicted curvature
+
       self.brake_clip = self.brake_actutator_target - self.brake_actutator_stdDevLow
 
     else:
