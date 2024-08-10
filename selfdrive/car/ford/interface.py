cb794a1952b0a88a2d0afd0f6d4d84ffd7e9e631
alan-polk <56047663+alan-polk@users.noreply.github.com>
2024-08-10 17:37:38 -0500

    dashcam mode sucks

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -22,8 +22,8 @@ class CarInterface(CarInterfaceBase):
     print(f'candidate (interface): {candidate}')
 
     ret.carName = "ford"
-    ret.dashcamOnly = not (ret.flags & FordFlags.CANFD)
-    print(f'Dashcam Only Mode: {ret.dashcamOnly}')
+    # ret.dashcamOnly = not (ret.flags & FordFlags.CANFD)
+    # print(f'Dashcam Only Mode: {ret.dashcamOnly}')
     ret.radarUnavailable = not (ret.flags & FordFlags.CANFD)
     print(f'Radar Unavailable: {ret.radarUnavailable}')
 
