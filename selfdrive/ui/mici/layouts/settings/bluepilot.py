99ceb23adfb53b400a7197efb4bbbefe37ca5ad8
John Christman <john@christmanzoo.com>
2026-01-02 15:30:22 -0500

    float button

--- selfdrive/ui/mici/layouts/settings/bluepilot.py
+++ selfdrive/ui/mici/layouts/settings/bluepilot.py
@@ -4,7 +4,7 @@ from collections.abc import Callable
 from openpilot.common.time_helpers import system_time_valid
 from openpilot.system.ui.widgets.scroller import Scroller
 from openpilot.selfdrive.ui.mici.widgets.button import BigButton, BigToggle, BigParamControl
-from openpilot.selfdrive.ui.mici.widgets.dialog import BigDialog, BigInputDialog
+from openpilot.selfdrive.ui.mici.widgets.floatbutton import BigParamFloatControl
 from openpilot.system.ui.lib.application import gui_app
 from openpilot.system.ui.widgets import NavWidget
 from openpilot.selfdrive.ui.layouts.settings.common import restart_needed_callback
@@ -22,41 +22,34 @@ class BluePilotLayoutMici(NavWidget):
     # ******** Main Scroller ********
     self.show_hands_free_ui = BigParamControl("show hands-free ui", "send_hands_free_cluster_msg")
     self.enable_human_turn_detection = BigParamControl("enable human turn detection", "enable_human_turn_detection")
+    self.lane_change_factor_high = BigParamFloatControl("lane change factor high", "lane_change_factor_high", "enter lane change factor high...")
+
+    self.pc_blend_ratio_high_C = BigParamFloatControl("pc blend ratio high C", "pc_blend_ratio_high_C_UI", "enter pc blend ratio high C...")
+    self.pc_blend_ratio_low_C = BigParamFloatControl("pc blend ratio low C", "pc_blend_ratio_low_C_UI", "enter pc blend ratio low C...")
+
     self.enable_lane_positioning = BigParamControl("enable lane positioning", "enable_lane_positioning")
+
+    self.custom_path_offset = BigParamFloatControl("custom path offset", "custom_path_offset", "enter custom path offset...")
+
+
     self.enable_adv_lane_pos = BigParamControl("enable adv. lane positioning", "enable_adv_lane_pos")
     self.enable_lane_full_mode = BigParamControl("enable lanefull mode", "enable_lane_full_mode")
     self.custom_profile = BigParamControl("enable custom profile", "custom_profile")
 
-    def lane_change_factor_high_clicked():
-      dlg = BigInputDialog("enter lane change factor high...", str(self.lane_change_factor_high),
-                           confirm_callback=lane_change_factor_high_callback, show_special_keys=True)
-      gui_app.set_modal_overlay(dlg)
-
-    def lane_change_factor_high_callback(password: str):
-      if password:
-        try:
-          self.lane_change_factor_high = float(password)
-          self._params.put_nonblocking("lane_change_factor_high", self.lane_change_factor_high)
-          update_lane_change_factor_high_btn()
-        except ValueError:
-          pass
-
-    def update_lane_change_factor_high_btn():
-      self.lane_change_factor_high_btn.set_text(f"lane change factor high [{round(self.lane_change_factor_high,4)}]")
-
-    self.lane_change_factor_high_btn = BigButton("", "")
-    update_lane_change_factor_high_btn()
-    self.lane_change_factor_high_btn.set_click_callback(lane_change_factor_high_clicked)
+    self.LC_PID_gain = BigParamFloatControl("LC PID gain UI", "LC_PID_gain_UI", "enter LC PID gain...")
 
     self._scroller = Scroller([
       self.show_hands_free_ui,
       self.enable_human_turn_detection,
-      self.lane_change_factor_high_btn,
+      self.lane_change_factor_high,
+      self.pc_blend_ratio_high_C,
+      self.pc_blend_ratio_low_C,
       self.enable_lane_positioning,
+      self.custom_path_offset,
       self.enable_adv_lane_pos,
       self.enable_lane_full_mode,
       self.custom_profile,
-
+      self.LC_PID_gain,
     ], snap_items=False)
 
     # Toggle lists
---
+++ selfdrive/ui/mici/widgets/floatbutton.py
@@ -0,0 +1,43 @@
+
+from openpilot.selfdrive.ui.mici.widgets.button import BigButton
+from openpilot.selfdrive.ui.mici.widgets.dialog import BigInputDialog
+from openpilot.common.params import Params
+from openpilot.system.ui.lib.application import gui_app, MousePos
+
+class BigParamFloatControl(BigButton):
+  def __init__(self, text: str, param: str, message: str):
+    self.label_text = text
+    self.message = message
+    super().__init__(text, "")
+    self.param = param
+    self.params = Params()
+    self.set_click_callback(self._on_click)
+    self.update_label()
+
+  def _on_click(self):
+    dlg = BigInputDialog(self.message, str(self.get_param()),
+                         confirm_callback=self._callback, show_special_keys=True)
+    gui_app.set_modal_overlay(dlg)
+
+  def _callback(self, password: str):
+    if password:
+      try:
+        float_value = float(password)
+        self.set_param(float_value)
+      except ValueError:
+        pass
+
+  def get_param(self) -> float:
+    try:
+      return float(self.params.get(self.param, return_default=True))
+    except (TypeError, ValueError):
+      return 0.0
+
+  def set_param(self, value: float):
+    self.params.put_nonblocking(self.param, value)
+    self.update_label(value)
+
+  def update_label(self, value: float = None):
+    if value is None:
+      value = self.get_param()
+    self.set_text(f"{self.label_text} [{round(value,4)}]")
