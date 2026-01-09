114b54b2d6a7cef64bdfe7a93bf2797ce6a7f0e8
John Christman <john@christmanzoo.com>
2026-01-08 19:30:19 -0500

    fix

--- cereal/services.py
+++ cereal/services.py
@@ -103,7 +103,7 @@ _services: dict[str, tuple] = {
   "liveLocationKalman": (True, 20.),
 
   # bluepilot
-  "torqueMeter": (False, 100., 10),
+  "carControllerBP": (False, 100., 10),
 
   # debug
   "uiDebug": (True, 0., 1),
