3ef2f0dfe44735c7613214c02f1f433e9d48ea98
alan-polk <56047663+alan-polk@users.noreply.github.com>
2026-01-19 16:22:30 -0600

    add bp portal to params

--- common/params_keys.h
+++ common/params_keys.h
@@ -283,4 +283,8 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"disable_BP_lat_UI", {PERSISTENT | BACKUP, BOOL, "0"}},
     {"vbatt_pause_charging", {PERSISTENT | BACKUP, FLOAT, "11.8"}},
     {"show_lead_speed", {PERSISTENT | BACKUP, BOOL, "1"}},
+
+    // BluePilot Portal (Web Routes Server)
+    {"BPPortalEnabled", {PERSISTENT | BACKUP, BOOL, "0"}},
+    {"BPPortalPort", {PERSISTENT | BACKUP, INT, "8088"}},
 };
