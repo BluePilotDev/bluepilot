cc4b31cc904c3bc28419bdb221a67eb369f3b0d4
alan-polk <56047663+alan-polk@users.noreply.github.com>
2026-02-12 13:56:23 -0600

    hardcode accel_pred to see if it is the issue

--- opendbc_repo/opendbc/car/ford/fordcan.py
+++ opendbc_repo/opendbc/car/ford/fordcan.py
@@ -148,7 +148,7 @@ def create_acc_msg(packer, CAN: CanBus, long_active: bool, gas: float, accel: fl
     "AccBrkTot_A_Rq": accel,                          # Brake total accel request: [-20|11.9449] m/s^2
     "Cmbb_B_Enbl": 1 if long_active else 0,           # Enabled: 0=No, 1=Yes
     "AccPrpl_A_Rq": gas,                              # Acceleration request: [-5|5.23] m/s^2
-    "AccPrpl_A_Pred": accel_pred,                     # From carcontroller (Ford stock: gas when not braking, else AccBrkTot)
+    "AccPrpl_A_Pred": -5.0 #accel_pred,                     # From carcontroller (Ford stock: gas when not braking, else AccBrkTot)
     "AccResumEnbl_B_Rq": 1 if long_active else 0,
     # No observed acceleration seen from this signal alone
     "AccVeh_V_Trg": v_ego_kph,                        # Target speed: [0|255] km/h
