408b1ecec07330146eed5493b2358227b038b00d
alan-polk <56047663+alan-polk@users.noreply.github.com>
2026-02-13 14:38:16 -0600

    how low can you go?

--- opendbc_repo/opendbc/car/ford/carcontroller.py
+++ opendbc_repo/opendbc/car/ford/carcontroller.py
@@ -907,9 +907,10 @@ class CarController(CarControllerBase): #, IntelligentCruiseButtonManagementInte
 
         # now let's apply some rate limits, to keep the places where we choose op_accel or op_gas from moving too fast
         # but only apply the limits if there is no imminent chance of a collision
+        UI_ROC_MODIFIER = 500 # to keep UI variables a reasonable number, divide them in carcontroller
         if ttc_sec > 8.0 and lead_time_sec > 0.5:
-          bp_gas = clip(bp_gas, self.bp_gas_last - self.following_gas_ROC/100, self.bp_gas_last + self.following_gas_ROC/100)
-          bp_accel = clip(bp_accel, self.bp_accel_last - self.following_accel_ROC/100, self.bp_accel_last + self.following_accel_ROC/100)
+          bp_gas = clip(bp_gas, self.bp_gas_last - self.following_gas_ROC/UI_ROC_MODIFIER, self.bp_gas_last + self.following_gas_ROC/UI_ROC_MODIFIER)
+          bp_accel = clip(bp_accel, self.bp_accel_last - self.following_accel_ROC/UI_ROC_MODIFIER, self.bp_accel_last + self.following_accel_ROC/UI_ROC_MODIFIER)
 
         # Set brake_actuate and precharge_actuate flags (initialized False above)
         if bp_accel < self.brake_actuate_target:
